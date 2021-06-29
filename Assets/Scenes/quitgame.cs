70032d2b06a47dedbc2d02b0d6a1b72cad79c7b5
NielsBron <79984300+NielsBron@users.noreply.github.com>
2021-06-29 17:19:37 +0200

    bomen
    bomen

---
+++ Assets/Scenes/quitgame.cs
@@ -0,0 +1,10 @@
+ using UnityEngine;
+ using System.Collections;
+ 
+ public class EndOflevel : MonoBehaviour {
+ 
+     void OnTriggerEnter2D(Collider2D other)
+     {
+         Application.Quit();
+     }
+ }
